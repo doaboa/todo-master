6da429f62dffe7131d108c74abe86ffd8c453cf2
Doa <doajafri@gmail.com>
2014-03-17 03:06:33 -0400

    created a filter

--- exercises/main.js
+++ exercises/main.js
@@ -1,9 +1,18 @@
 //create an app, dependencies go in [], currently none
 var firstApp = angular.module('firstApp', []);
 
-//give app a factory to pull from, name it and put data in function
+//give app a factory to pull from, name it and put data in function. this is a service
 firstApp.factory('Data', function() {
 	return {message:"I'm data from a service"}
+})
+
+//first line names filter and (Data) is designated if we want to inject
+firstApp.filter('mirror', function(Data) {
+	//takes input of the filter
+	return function(text) {
+		//this is what the filter actually does, plus injected info
+		return text.split("").reverse().join("") + " " + Data.message;
+	}
 })
 
 //scope is always a dependency, also list factory as dependency
@@ -14,7 +23,8 @@ function FirstCtrl($scope, Data) {
 function SecondCtrl($scope, Data) {
 	$scope.data = Data;
 
-	$scope.backwardsMsg = function (message) {
-		return message.split("").reverse().join("");
-	}
+	// a filter replaces the logic below to make it more reusable
+	// $scope.backwardsMsg = function (message) {
+	// 	return message.split("").reverse().join("");
+	// }
 }
