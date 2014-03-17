d044d6029b1bca9387ffe0b387c02e797580d507
Doa <doajafri@gmail.com>
2014-03-17 01:46:04 -0400

    set up a service and injected bindable text from main.js by using a factory

--- exercises/main.js
+++ exercises/main.js
@@ -1,6 +1,16 @@
-function FirstCtrl($scope) {
+//create an app, dependencies go in [], currently none
+var firstApp = angular.module('firstApp', []);
+
+//give app a factory to pull from, name it and put data in function
+firstApp.factory('Data', function() {
+	return {message:"I'm data from a service"}
+})
+
+//scope is always a dependency, also list factory as dependency
+function FirstCtrl($scope, Data) {
+	$scope.data = Data;
 }
 
-function SecondCtrl($scope) {
-
+function SecondCtrl($scope, Data) {
+	$scope.data = Data;
 }
